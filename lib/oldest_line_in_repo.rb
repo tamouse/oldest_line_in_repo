6ab77f2a6af561549e2c34aeb3a85efe34df0411
Tamara Temple <tamouse@gmail.com>
2017-08-23 11:20:54 -0500

    create Ops class

--- lib/oldest_line_in_repo.rb
+++ lib/oldest_line_in_repo.rb
@@ -1,5 +1,37 @@
+require "open3"
+require "rugged"
 require "oldest_line_in_repo/version"
 
 module OldestLineInRepo
-  # Your code goes here...
+  class Ops
+    attr_accessor :repo_root, :repo
+
+    def initialize(repo_root='.')
+      self.repo_root = repo_root
+      self.repo = open_repo(repo_root)
+    end
+
+    def open_repo(repo_root)
+      Rugged::Repository.new(repo_root)
+    end
+
+    def git_files
+      Dir.chdir(repo_root) do |dir|
+        cmd='git ls-files'
+        o,e,s = Open3.capture3(cmd)
+        unless s.success?
+          $stderr.write "Error from #{cmd}:"
+          $stderr.write e
+          raise "Error running #{cmd}"
+        end
+        o
+      end
+    end
+
+    def blame(file)
+      Rugged::Blame.new(repo, file)
+    end
+
+  end
+
 end
--- test/oldest_line_in_repo_test.rb
+++ test/oldest_line_in_repo_test.rb
@@ -1,11 +1,48 @@
 require "test_helper"
 
 class OldestLineInRepoTest < Minitest::Test
+  def setup
+    @project_root = File.expand_path('../../', __FILE__)
+    @bogus_root = File.expand_path(File.dirname(__FILE__))
+    @ops = ::OldestLineInRepo::Ops.new(@project_root)
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::OldestLineInRepo::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_it_finds_a_repo
+    refute_nil @ops.open_repo(@project_root)
   end
+
+  def test_it_fails_without_a_repo
+    assert_raises Rugged::RepositoryError do
+      @ops.open_repo(@bogus_root)
+    end
+  end
+
+  def test_git_files
+    file_list = @ops.git_files
+    refute_nil file_list
+    files = file_list.split("\n")
+    assert_includes(files, 'oldest_line_in_repo.gemspec')
+  end
+
+  def test_failing_git_files
+    old_root = @ops.repo_root
+    @ops.repo_root = 'xyzzy'
+    assert_raises Errno::ENOENT do
+      @ops.git_files
+    end
+    @ops.repo_root = old_root
+  end
+
+  def test_blame
+    blame_data = @ops.blame('oldest_line_in_repo.gemspec')
+    refute_nil blame_data
+    require "byebug"; byebug
+    assert_kind_of Hash, blame_data, blame_data.inspect
+  end
+
+
 end
