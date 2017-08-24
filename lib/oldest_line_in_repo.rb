98c6ba34997bb4f1eeeca9291f352ccee478d8d1
Tamara Temple <tamouse@gmail.com>
2017-08-23 23:32:29 -0500

    Move  class to it's own file in

--- lib/oldest_line_in_repo.rb
+++ lib/oldest_line_in_repo.rb
@@ -1,55 +1,7 @@
 require "open3"
 require "rugged"
 require "oldest_line_in_repo/version"
+require "oldest_line_in_repo/ops"
 
 module OldestLineInRepo
-  class Ops
-    attr_accessor :repo_root, :repo
-
-    def initialize(repo_root='.')
-      self.repo_root = repo_root
-      self.repo = open_repo(repo_root)
-    end
-
-    def open_repo(repo_root)
-      Rugged::Repository.new(repo_root)
-    end
-
-    def git_files
-      Dir.chdir(repo_root) do |dir|
-        cmd='git ls-files'
-        o,e,s = Open3.capture3(cmd)
-        unless s.success?
-          $stderr.write "Error from #{cmd}:"
-          $stderr.write e
-          raise "Error running #{cmd}"
-        end
-        o.split("\n")
-      end
-    end
-
-    def blame(file)
-      Rugged::Blame.new(repo, file)
-    end
-
-    def oldest_line_in_file(blame_data)
-      blame_data.min_by do |x|
-        x[:final_signature][:time]
-      end
-    end
-
-    def oldest_lines_in_repo
-      git_files.map do |file|
-        oldest_line_in_file(blame(file))
-      end
-    end
-
-    def oldest_file_in_repo
-      oldest_lines_in_repo.min_by do |x|
-        x[:final_signature][:time]
-      end
-    end
-
-  end
-
 end
---
+++ test/oldest_line_in_repo/ops_test.rb
@@ -0,0 +1,60 @@
+require "test_helper"
+
+class OldestLineInRepo::OpsTest < Minitest::Test
+  def setup
+    @test_file = 'oldest_line_in_repo.gemspec'
+    @project_root = File.expand_path('../../../', __FILE__)
+    @bogus_root = File.expand_path(File.dirname(__FILE__))
+    @ops = ::OldestLineInRepo::Ops.new(@project_root)
+  end
+
+  def test_it_finds_a_repo
+    refute_nil @ops.open_repo(@project_root)
+  end
+
+  def test_it_fails_without_a_repo
+    assert_raises Rugged::RepositoryError do
+      @ops.open_repo(@bogus_root)
+    end
+  end
+
+  def test_git_files
+    files = @ops.git_files
+    assert_includes(files, @test_file)
+  end
+
+  def test_failing_git_files
+    old_root = @ops.repo_root
+    @ops.repo_root = 'xyzzy'
+    assert_raises Errno::ENOENT do
+      @ops.git_files
+    end
+    @ops.repo_root = old_root
+  end
+
+  def test_blame
+    blame_data = @ops.blame(@test_file)
+    assert_kind_of Rugged::Blame, blame_data, blame_data.inspect
+  end
+
+  def test_oldest_line_in_file
+    blame_data = @ops.blame(@test_file)
+    oldest = @ops.oldest_line_in_file(blame_data)
+    assert_kind_of Hash, oldest, oldest.inspect
+    assert_equal oldest[:final_signature][:time].strftime("%F-%H-%M"), "2017-08-23-09-07"
+  end
+
+  def test_oldest_lines_in_repo
+    oldest = @ops.oldest_lines_in_repo
+    assert_kind_of Array, oldest
+    assert_equal oldest.count, @ops.git_files.count
+  end
+
+  def test_oldest_file_in_repo
+    oldest = @ops.oldest_file_in_repo
+    assert_kind_of Hash, oldest
+    assert_equal oldest[:final_signature][:time].strftime("%F-%H-%M"), "2017-08-23-09-07"
+  end
+
+
+end
